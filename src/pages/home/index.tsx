db3bdcd18fdc4817f47fa2847ce87484ab04d353
amirzenoozi <amirzenoozi72@gmail.com>
2023-12-22 15:22:59 +0100

    :sparkles: Feat: Add Skills Section

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,9 +5,36 @@ import FlexCol from '../../components/flex-col';
 import FlexRow from '../../components/flex-row';
 import Hyperlink from '../../components/hyperlink';
 import Linkedin from '../../icons/linkedin';
+import JavaScript from '../../icons/JavaScript';
+import TypeScript from '../../icons/TypeScript';
+import Python from '../../icons/Python';
+import Bash from '../../icons/Bash';
+import ReactJs from '../../icons/React';
+import Angular from '../../icons/Angular';
+import WordPress from '../../icons/WordPress';
+import D3 from '../../icons/D3';
+import ChartJs from '../../icons/ChartJs';
+import Notification from '../../icons/Notification';
+import VueJs from '../../icons/VueJs';
+import Gulp from '../../icons/Gulp';
+import Webpack from '../../icons/Webpack';
+import Flask from '../../icons/Flask';
+import ExpressJs from '../../icons/ExpressJs';
+import FastAPI from '../../icons/FastAPI';
+import GraphQL from '../../icons/GraphQL';
+import RestAPI from '../../icons/RestAPI';
+import MariaDB from '../../icons/MariaDB';
+import Elementor from '../../icons/Elementor';
+import MongoDB from '../../icons/MongoDB';
 import Icon from '@icon-park/react/es/all';
 import links from '../../constants/links.json';
 import './style.scss';
+import Selenium from '../../icons/Selenium';
+import UnitTest from '../../icons/UnitTest';
+import Jest from '../../icons/Jest';
+import Docker from '../../icons/Docker';
+import GitLab from '../../icons/GitLab';
+import Actions from '../../icons/Actions';
 
 
 interface Experience {
@@ -84,6 +111,142 @@ function Home() {
 			date: 'Apr. 2016 - Nov. 2016',
 			translation_key: 'rabin',
 			objectives: rabinObjectives,
+		}
+	];
+	const skillsIconSize: number = 80;
+	const skillsList: Array<any> = [
+		{
+			title: 'Programming Languages',
+			items: [
+				{
+					title: 'TypeScript',
+					icon: <TypeScript size={skillsIconSize} />,
+				},
+				{
+					title: 'JavaScript',
+					icon: <JavaScript size={skillsIconSize} />,
+				},
+				{
+					title: 'Python',
+					icon: <Python size={skillsIconSize} />,
+				},
+				{
+					title: 'Bash',
+					icon: <Bash size={skillsIconSize} />,
+				},
+			]
+		},
+		{
+			title: 'Frameworks & Libraries',
+			items: [
+				{
+					title: 'React',
+					icon: <ReactJs size={skillsIconSize} />,
+				},
+				{
+					title: 'Angular',
+					icon: <Angular size={skillsIconSize} />,
+				},
+				{
+					title: 'VueJs',
+					icon: <VueJs size={skillsIconSize} />,
+				},
+				{
+					title: 'WordPress',
+					icon: <WordPress size={skillsIconSize} />,
+				},
+				{
+					title: 'Elementor',
+					icon: <Elementor size={skillsIconSize} />,
+				},
+				{
+					title: 'D3',
+					icon: <D3 size={skillsIconSize} />,
+				},
+				{
+					title: 'Chart.js',
+					icon: <ChartJs size={skillsIconSize} />,
+				},
+				{
+					title: 'WebSocket',
+					icon: <Notification size={skillsIconSize} />,
+				},
+				{
+					title: 'Gulp',
+					icon: <Gulp size={skillsIconSize} />,
+				},
+				{
+					title: 'Webpack',
+					icon: <Webpack size={skillsIconSize} />,
+				}
+			]
+		},
+		{
+			title: 'Back-End',
+			items: [
+				{
+					title: 'ExpressJs',
+					icon: <ExpressJs size={skillsIconSize} />,
+				},
+				{
+					title: 'FastAPI',
+					icon: <FastAPI size={skillsIconSize} />,
+				},
+				{
+					title: 'GraphQL',
+					icon: <GraphQL size={skillsIconSize} />,
+				},
+				{
+					title: 'RestAPI',
+					icon: <RestAPI size={skillsIconSize} />,
+				},
+				{
+					title: 'Flask',
+					icon: <Flask size={skillsIconSize} />,
+				},
+				{
+					title: 'MariaDB',
+					icon: <MariaDB size={skillsIconSize} />,
+				},
+				{
+					title: 'MongoDB',
+					icon: <MongoDB size={skillsIconSize} />,
+				},
+			]
+		},
+		{
+			title: 'Testing',
+			items: [
+				{
+					title: 'Cypress',
+					icon: <UnitTest size={skillsIconSize} />,
+				},
+				{
+					title: 'Selenium',
+					icon: <Selenium size={skillsIconSize} />,
+				},
+				{
+					title: 'Jest',
+					icon: <Jest size={skillsIconSize} />,
+				},
+			]
+		},
+		{
+			title: 'DevOps',
+			items: [
+				{
+					title: 'Docker',
+					icon: <Docker size={skillsIconSize} />,
+				},
+				{
+					title: 'GitLab CI',
+					icon: <GitLab size={skillsIconSize} />,
+				},
+				{
+					title: 'Actions',
+					icon: <Actions size={skillsIconSize} />,
+				},
+			]
 		}
 	]
 
@@ -162,6 +325,7 @@ function Home() {
 						</FlexCol>
 					</FlexRow>
 				</Container>
+				<span className={'hero-mouse'}/>
 			</section>
 			<section className={'experience'}>
 				<span className={'experience-circle experience-circle--one'} />
@@ -186,6 +350,29 @@ function Home() {
 									</li>
 								))}
 							</ul>
+						</div>
+					))}
+				</Container>
+			</section>
+			<section className={'skill'}>
+				<span className={'skill-circle skill-circle--one'} />
+				<span className={'skill-circle skill-circle--two'} />
+				<Container>
+					{skillsList.map((section: any) => (
+						<div className={'skill-section'}>
+							<h2>{section.title}:</h2>
+							<FlexRow>
+								{section.items.map((item: any) => (
+									<FlexCol xs={12} sm={8} md={6} lg={4} xl={3}>
+										<div className={'skill-item'}>
+											<div className={'skill-content'}>
+												{item.icon}
+												<p>{item.title}</p>
+											</div>
+										</div>
+									</FlexCol>
+								))}
+							</FlexRow>
 						</div>
 					))}
 				</Container>

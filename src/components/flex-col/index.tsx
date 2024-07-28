d99b5abf07b11e27de76266862fe2c752e496e1a
amirzenoozi <amirzenoozi72@gmail.com>
2024-07-28 20:17:40 +0200

    :bug: Fix: Solve ClassNames Issues

--- src/components/flex-col/index.tsx
+++ src/components/flex-col/index.tsx
@@ -1,43 +1,33 @@
-import React from 'react';
-import './style.scss';
+import React from 'react'
+import './style.scss'
+import type FlexColProps from './type'
 
-interface FlexColProps {
-	xs?: number;
-	sm?: number;
-	md?: number;
-	lg?: number;
-	xl?: number;
-	xxl?: number;
-	center?: boolean;
-	children: React.ReactNode;
-}
-
-const FlexCol: React.FC<FlexColProps> = ({ xs = 24, sm = 0, md = 0, lg = 0, xl = 0, xxl = 0, center = false, children }) => {
+const FlexCol = ({ xs = 24, sm = 0, md = 0, lg = 0, xl = 0, xxl = 0, center = false, children }: FlexColProps) => {
 	return (
-		<div className={[center && 'col--center', colClassname(xs, sm, md, lg, xl, xxl)].join(' ')}>
+		<div className={['col', ...center ? ['col--center'] : [], colClassname(xs, sm, md, lg, xl, xxl)].join(' ')}>
 			{ children }
 		</div>
-	);
+	)
 }
 
-const  colClassname = (xs: number, sm: number, md: number, lg: number, xl: number, xxl: number): string => {
-	const classes: string[] = [];
-	classes.push( generatorClass(xs, 'xs') );
-	classes.push( generatorClass(sm, 'sm') );
-	classes.push( generatorClass(md, 'md') );
-	classes.push( generatorClass(lg, 'lg') );
-	classes.push( generatorClass(xl, 'xl') );
-	classes.push( generatorClass(xxl, 'xxl') );
+const colClassname = (xs: number, sm: number, md: number, lg: number, xl: number, xxl: number): string => {
+	const classes: string[] = []
+	classes.push(generatorClass(xs, 'xs'))
+	classes.push(generatorClass(sm, 'sm'))
+	classes.push(generatorClass(md, 'md'))
+	classes.push(generatorClass(lg, 'lg'))
+	classes.push(generatorClass(xl, 'xl'))
+	classes.push(generatorClass(xxl, 'xxl'))
 
-	return classes.join(' ');
+	return classes.join(' ')
 }
 
 const generatorClass = (size: number, breackpoint: string) => {
 	if (typeof size === 'undefined' || size === null) {
-		return '';
+		return ''
 	}
 
-	return size === 0 ? '' : `col-${breackpoint}-${size}`;
+	return size === 0 ? '' : `col-${breackpoint}-${size}`
 }
 
-export default FlexCol;
+export default FlexCol
---
+++ src/components/flex-col/type.ts
@@ -0,0 +1,14 @@
+import type React from 'react'
+
+interface FlexColProps {
+	xs?: number
+	sm?: number
+	md?: number
+	lg?: number
+	xl?: number
+	xxl?: number
+	center?: boolean
+	children: React.ReactNode
+}
+
+export default FlexColProps

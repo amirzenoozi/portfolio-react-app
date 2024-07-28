85ac9ad567ffaf8bc7e8e51154650e77230c6d63
amirzenoozi <amirzenoozi72@gmail.com>
2024-07-28 21:48:42 +0200

    :fire: Chore: Remove Extra Imports

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -2,15 +2,13 @@ import React, { useEffect } from 'react';
 import Header from '@/components/header';
 import { setLanguage } from '@/store/uiSettings/uiSettingsSlice';
 import { Outlet, useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '@/store';
+import { useDispatch } from 'react-redux';
 import './style.scss';
 import i18n from 'i18next';
 
 const Layout = () => {
 	const { lng } = useParams<{ lng: string }>();
 	const dispatch = useDispatch();
-	const currentLanguage = useSelector((state: RootState) => state.uiSettings.currentLanguage);
 
 	useEffect(() => {
 		if (lng != null) {

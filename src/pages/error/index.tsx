96609517a3dee79d44e18a34db6948ac088b24cc
amirzenoozi <amirzenoozi72@gmail.com>
2024-07-28 21:25:48 +0200

    :zap: Feat: Use ClassGenerator For Error Page

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './style.scss';
 import { Link } from 'react-router-dom';
+import { ClassNames } from '@/modules/classNames';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/store';
 
 const Error = () => {
-  return (
-    <div className="error">
-        <h1>Oops, Something Went Wrong!</h1>
-        <p>Try refreshing the page or going back to the <Link to={'/'}>Home</Link> Page.</p>
-    </div>
-  );
+	const CN = new ClassNames('error');
+    const currentLanguage = useSelector((state: RootState) => state.uiSettings.currentLanguage);
+
+    return (
+		<div className={CN.generate()}>
+			<h1>Oops, Something Went Wrong!</h1>
+			<p>Try refreshing the page or going back to the <Link to={`/${currentLanguage}`}>Home</Link> Page.</p>
+		</div>
+	);
 }
 
 export default Error;
